48b8fff14542a32bbd2529145b068b3cd8cd649c
qfdasfdsa <fdasf>
2026-07-17 13:44:02 +0700

    Initial Commit

---
+++ MemoryBox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemoryBox
+//
+//  Created by QuanDX1 on 17/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemoryBox/MemoryBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryBoxApp.swift
+//  MemoryBox
+//
+//  Created by QuanDX1 on 17/7/26.
+//
+
+import SwiftUI
+
+@main
+struct MemoryBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
